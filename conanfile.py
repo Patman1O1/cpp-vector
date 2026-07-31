00a06a77984e60840ba2709315490958181df182
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-31 17:04:59 -0500

    Remove cpp prefix

--- conanfile.py
+++ conanfile.py
@@ -2,8 +2,8 @@ from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 import os
 
-class CppVector(ConanFile):
-    name = "cpp_vector"
+class Vector(ConanFile):
+    name = "vector"
     version = "0.1.0"
     author = "Patman1O1"
     description = ""
@@ -44,6 +44,6 @@ class CppVector(ConanFile):
     def package(self) -> None: CMake(self).install()
 
     def package_info(self) -> None:
-        self.cpp_info.set_property("cmake_target_name", "collections::cpp_vector")
+        self.cpp_info.set_property("cmake_target_name", "collections::vector")
         self.cpp_info.bindirs = []
         self.cpp_info.libdirs = []
