2875629f92d5369b2a2ce1a9477a8f6212176c05
Patrick Thomas <patrickthomas101x@gmail.com>
2026-08-29 09:50:09 -0500

    Implement test category namespaces

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -6,5 +6,23 @@
 #include <collections/vector.hpp>
 
 namespace collections::vector_testing {
+    namespace constructor_testing {
 
+    } // namespace constructor_testing
+
+    namespace destructor_testing {
+
+    } // namespace destructor_testing
+    
+    namespace operator_testing {
+
+    } // namespace operator_testing
+
+    namespace method_testing {
+
+    } // namespace method_testing
+
+    namespace deduction_guide_testing {
+
+    } // namespace deduction_guide_testing
 } // namespace collections::vector_testing
